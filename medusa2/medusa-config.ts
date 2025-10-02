6abcf3a9bc0c1f977f5fbf7f946a8850bd7e64a2
jesusparfer27 <jesusparfer@alumnos.cei.es>
2025-10-02 12:34:33 +0200

    commit

--- medusa2/medusa-config.ts
+++ medusa2/medusa-config.ts
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
+      storeCors: process.env.STORE_CORS!, 
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
